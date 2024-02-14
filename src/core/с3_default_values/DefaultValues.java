fa5b70bf1a6a848736a5185a4f9c1952ef1c63b6
Maxim Mironov <maksim_mironov01@bk.ru>
2024-02-14 20:57:30 +0400

    Какими значениями инициализируются переменные по умолчанию?

---
+++ src/core/с3_default_values/DefaultValues.java
@@ -0,0 +1,14 @@
+package core.с3_default_values;
+
+public class DefaultValues {
+    byte byteField = (byte)0;
+    short shortField = (short)0;
+    int intField = 0;
+    long longField = 0L;
+    float floatField = 0f;
+    double doubleField = 0d;
+    char charField = '\u0000';
+    boolean booleanField = false;
+    Object anyObjectField = null;
+
+}
