7491cf45639d719e89429c582edb73181831dff2
Maxim Mironov <maksim_mironov01@bk.ru>
2024-02-16 15:16:47 +0400

    Дайте определение понятию «интерфейс». Какие модификаторы по умолчанию имеют поля и методы интерфейсов?

---
+++ src/core/c10_interface/Class.java
@@ -0,0 +1,20 @@
+package core.c10_interface;
+
+public class Class implements Interface{
+    @Override
+    public String method(String string) {
+        return string;
+    }
+    @Override
+    public void defaultMethod() {
+        Interface.super.defaultMethod();
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Class clas = new Class();
+        Interface.staticMethod();
+        clas.defaultMethod();
+    }
+}
