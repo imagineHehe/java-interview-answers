7a1767cb258dd8efe7ab43401f4133a6d1fbddf1
Maxim Mironov <maksim_mironov01@bk.ru>
2024-02-22 13:47:11 +0400

    Может ли статический метод быть переопределён или перегружен?

---
+++ src/core/c22_c30/c22_override_and_overload_static_method/Main.java
@@ -0,0 +1,8 @@
+package core.c22_c30.c22_override_and_overload_static_method;
+
+class Main {
+    public static void main(String[] args) {
+        Parent.staticMethod();
+        Child.staticMethod();
+    }
+}
--- src/core/c22_c30/c22_override_and_overload_static_method/Parent.java
+++ src/core/c22_c30/c22_override_and_overload_static_method/Parent.java
@@ -1,5 +1,11 @@
 package core.c22_c30.c22_override_and_overload_static_method;
 
 class Parent {
-
+    public static void staticMethod(){
+        System.out.println("nothingInParent");
+    }
+    //Перегрузка статических методов доступна без ограничений
+    public static void staticMethod(String string){
+        System.out.println(string);
+    }
 }
