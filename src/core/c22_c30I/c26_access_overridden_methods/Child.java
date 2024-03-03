9f34ac1449aa48b78a6d175bdcdb263eefa725a7
Maxim Mironov <maksim_mironov01@bk.ru>
2024-03-03 14:36:00 +0400

    Как получить доступ к переопределенным методам родительского класса?

---
+++ src/core/c22_c30I/c26_access_overridden_methods/Child.java
@@ -0,0 +1,7 @@
+package core.c22_c30I.c26_access_overridden_methods;
+
+public class Child extends Parent{
+    public void callParentMethod() {
+        super.parentMethod();
+    }
+}
---
+++ src/core/c22_c30I/c26_access_overridden_methods/Parent.java
@@ -0,0 +1,7 @@
+package core.c22_c30I.c26_access_overridden_methods;
+
+class Parent {
+    public void parentMethod() {
+        System.out.println("Parent method");
+    }
+}
